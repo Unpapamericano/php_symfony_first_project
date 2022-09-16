3bf307ab21867c2ddc506a43966bbaf50ce5396f
Unpapamericano <donchogh17@gmail.com>
2022-09-17 00:16:52 +0200

    adding project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+
+        print_r('home page');
+        exit();
+
+        /*return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);*/
+    }
+
+    #[Route('/clients', name: 'app_clients')]
+    public function clients(): Response
+    {
+
+        print_r('clients page');
+        exit();
+
+        /*return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);*/
+    }
+}
